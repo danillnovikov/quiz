66345b0e8ecc5f9ed24d152e2fdc1c1a44dd8ec5
Daniil <danill.novikov@mail.ru>
2022-10-01 08:41:27 +0300

    divided into components file

--- src/App.js
+++ src/App.js
@@ -1,77 +1,9 @@
 import './index.scss';
 import React from 'react';
+import Game from './components/Game';
+import Result from './components/Result';
+import { questions } from './questions/questionsReact';
 import { useState } from 'react';
-
-const questions = [
-  {
-    title: 'React - это ...? ',
-    aswers: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    aswers: [
-      'приложение',
-      'часть приложения или страницы',
-      'то, что я не знаю что такое',
-    ],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    aswers: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
-
-function Game({ question, onClickVariant, step }) {
-  const percentage = Math.round((step / questions.length) * 100);
-
-  return (
-    <>
-      <div className="progress">
-        <div
-          style={{ width: `${percentage}%` }}
-          className="progress__inner"
-        ></div>
-      </div>
-
-      <h1>{question.title} </h1>
-
-      <ul>
-        {question.aswers.map((text, index) => {
-          return (
-            <li onClick={() => onClickVariant(index)} key={text}>
-              {text}
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
-}
-
-function Result({ correct }) {
-  // console.log(questions.length);
-  return (
-    <div className="result">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png"
-        alt="img"
-      />
-      <h2>
-        Вы отгадали {correct} ответа из {questions.length}
-      </h2>
-      <a href="/">
-        <button>Попробовать снова</button>
-      </a>
-    </div>
-  );
-}
 
 function App() {
   const [step, setStep] = useState(0);
@@ -88,9 +20,14 @@ function App() {
   return (
     <div className="App">
       {step !== questions.length ? (
-        <Game step={step} question={question} onClickVariant={onClickVariant} />
+        <Game
+          step={step}
+          question={question}
+          onClickVariant={onClickVariant}
+          questions={questions}
+        />
       ) : (
-        <Result correct={correct} />
+        <Result correct={correct} questions={questions} />
       )}
     </div>
   );
---
+++ src/components/Game.jsx
@@ -0,0 +1,28 @@
+const Game = ({ question, onClickVariant, step, questions }) => {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+
+      <h1>{question.title} </h1>
+
+      <ul>
+        {question.aswers.map((text, index) => {
+          return (
+            <li onClick={() => onClickVariant(index)} key={text}>
+              {text}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Game;
